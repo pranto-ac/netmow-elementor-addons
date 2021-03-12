8acd613527e01db0730bc2ea478b2974de31741f
unknown <pranto.netmow@gmail.com>
2021-03-12 14:37:44 +0600

    first files

---
+++ netmow-elementor-addons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Netmow Elementor Addons
+ * Plugin URI: https://netmow.com/
+ * Description: A plugin for custom elementor widgets.
+ * Version: 1.0.0
+ * Author: Netmow
+ * Author URI: https://netmow.com/
+ * Text Domain: netmow
+ * Domain Path: /languages
+ * License: GPL2
+ */
